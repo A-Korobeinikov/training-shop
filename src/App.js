75bd22414922611c02dbd01276a2ba798f1fdf4f
Anton Korobeinikov <an.korobeinikov@mail.ru>
2022-02-16 14:19:33 +0300

    добавил блок subscribe

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 import Promo from './components/Promo/Promo';
-// import Subscribe from './components/subscribe/Subscribe';
+import Subscribe from './components/subscribe/Subscribe';
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Main /> 
       <Promo />
-      {/* <Subscribe /> */}
+      <Subscribe />
       <Footer />
     </div>
 
