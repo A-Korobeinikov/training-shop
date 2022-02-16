4a3dbc5b96c9f4ed87cbf16fd03f4af640108150
Anton Korobeinikov <an.korobeinikov@mail.ru>
2022-02-16 20:02:50 +0300

    компонента Products

---
+++ src/components/products/women/Women.js
@@ -0,0 +1,161 @@
+import React from "react";
+import "./Women.css";
+import women from "./../products-img/women1.jpg";
+import star from "./../products-img/star-ico.svg";
+
+
+const ProductWomen = () => {
+    return (
+        <div className="product-women">
+            <div className="content">
+                <div className="product-description">
+                    <div className="title-product">
+                        <a href="#top" className="link-product">WOMEN’S</a>
+                    </div>
+                    <div className="sort-product">
+                        <button className="btn-product">NEW ARRIVALS</button>
+                        <button className="btn-product">SPECIALS</button>
+                        <button className="btn-product">BESTSELLERS</button>
+                        <button className="btn-product">MOST VIEWED</button>
+                        <button className="btn-product">FEATURED PRODUCTS</button>
+                    </div>
+                </div>
+                <div className="product">
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                    <a href="#top" class="product-link">
+                    <div class="product-card">
+                        <img src={women} alt="product card" />
+                    <p class="product-name"> Women’s tracksuit Q109</p>
+                        <div class="product-info">
+                            <p class="product-price"> $ 30.00</p>
+                            <div class="product-star">
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                                <img src={star} alt="star" />
+                            </div>
+                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div className="all-product">
+                    <button className="btn-all-product">See All</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductWomen;
